c341a00a6c28aab4265d4f0cebb19e655b7e3d04
zlikun <zlikun-dev@hotmail.com>
2017-06-23 14:30:52 +0800

    初始化spring-boot核心学习工程(模块)

---
+++ zlikun-spring-boot-core/src/main/java/com/zlikun/spring/SpringBootCoreApplication.java
@@ -0,0 +1,12 @@
+package com.zlikun.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCoreApplication.class, args);
+	}
+}
