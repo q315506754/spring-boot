cace52a8a27c5103d9a338346cbb2ede922a405b
zlikun <zlikun-dev@hotmail.com>
2017-06-23 14:50:32 +0800

    修改启动Banner

--- zlikun-spring-boot-core/src/main/java/com/zlikun/spring/SpringBootCoreApplication.java
+++ zlikun-spring-boot-core/src/main/java/com/zlikun/spring/SpringBootCoreApplication.java
@@ -3,10 +3,23 @@ package com.zlikun.spring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @SpringBootApplication 标记的类通常包含一个main()，是程序的入口 <br>
+ *     该注解是spring-boot的核心注解，是一个组合注解，包含：
+ *     @Configuration
+ *     @EnableAutoConfiguration
+ *     @ComponentScan
+ *     所以不使用该注解，直接使用上面三个注解也能实现相同效果
+ *     该注解默认扫描注解所在类的同级包，所以这里实际可以不配置扫描包
+ *     `exclude`参数则用于关闭特定的自定配置类，如：DataSourceAutoConfiguration
+ */
 @SpringBootApplication
+//@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+//@SpringBootApplication(scanBasePackages = {"com.zlikun.spring"})
 public class SpringBootCoreApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootCoreApplication.class, args);
 	}
+
 }
