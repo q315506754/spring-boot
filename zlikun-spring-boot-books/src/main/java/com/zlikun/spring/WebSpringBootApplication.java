71a80e4fc65fae2a73007486daf250fce466edf0
zlikun <zlikun-dev@hotmail.com>
2017-06-28 09:25:32 +0800

    测试事务提交和回滚(单元测试)

--- zlikun-spring-boot-books/src/main/java/com/zlikun/spring/WebSpringBootApplication.java
+++ zlikun-spring-boot-books/src/main/java/com/zlikun/spring/WebSpringBootApplication.java
@@ -2,9 +2,11 @@ package com.zlikun.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
+@EnableTransactionManagement
 public class WebSpringBootApplication extends WebMvcConfigurerAdapter {
 
 	public static void main(String[] args) {
--- zlikun-spring-boot-books/src/test/java/com/zlikun/spring/dao/BorrowDaoTest.java
+++ zlikun-spring-boot-books/src/test/java/com/zlikun/spring/dao/BorrowDaoTest.java
@@ -5,15 +5,18 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.transaction.TransactionAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * @author zlikun <zlikun-dev@hotmail.com>
  * @date 2017/6/25 15:23
  */
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes = {DaoConfig.class , DataSourceAutoConfiguration.class})
+@SpringBootTest(classes = {DaoConfig.class ,DataSourceAutoConfiguration.class})
 public class BorrowDaoTest {
 
     @Autowired
@@ -24,7 +27,7 @@ public class BorrowDaoTest {
         Assert.assertTrue(borrowDao.isFree(1024L));
     }
 
-    @Test
+    @Test @Transactional @Rollback(true)
     public void borrow() {
         Assert.assertTrue(borrowDao.borrow(1L ,1L ,7 * 24 * 3600L));
     }
