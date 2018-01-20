a816d5a5cf4b0ff98d5192ce16bb227353ce8179
zlikun <zlikun-dev@hotmail.com>
2018-01-20 14:46:18 +0800

    spring-boot 默认启用了 cacheManager ，不需要重复配置(如果配置，默认的将不生效)

--- spring-boot-redis/src/main/java/com/zlikun/spring/conf/CacheConfigure.java
+++ spring-boot-redis/src/main/java/com/zlikun/spring/conf/CacheConfigure.java
@@ -1,12 +1,8 @@
 package com.zlikun.spring.conf;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.core.RedisTemplate;
 
 /**
@@ -20,11 +16,12 @@ public class CacheConfigure {
     @Autowired
     private RedisTemplate redisTemplate;
 
-    @Bean @Primary
-    public CacheManager cacheManager() {
-        RedisCacheManager cacheManager = new RedisCacheManager(redisTemplate);
-        cacheManager.setDefaultExpiration(15);  // 为了便于测试，缓存默认过期时间为15秒
-        return cacheManager;
-    }
+//    // 与 org.springframework.boot.autoconfigure.cache.RedisCacheConfiguration 类功能一致(默认有配置，除非有特定需求，否则无需配置)
+//    @Bean
+//    public CacheManager cacheManager() {
+//        RedisCacheManager cacheManager = new RedisCacheManager(redisTemplate);
+//        cacheManager.setDefaultExpiration(15);  // 为了便于测试，缓存默认过期时间为15秒
+//        return cacheManager;
+//    }
 
 }
